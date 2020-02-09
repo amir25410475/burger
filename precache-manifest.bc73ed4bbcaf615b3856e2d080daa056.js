aa2a47ec7eb2b014328873f9b00c47bd5f74a814
Amir <amir25410475@gmail.com>
2020-02-09 14:48:52 +0600

    Updates

---
+++ precache-manifest.bc73ed4bbcaf615b3856e2d080daa056.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53041c618bc62ee947d642c3736f0c7e",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "7af92d8e0ff22f7915a8",
+    "url": "/burger/static/css/main.8a27fe7b.chunk.css"
+  },
+  {
+    "revision": "7d0b6dd3dacb9c091a81",
+    "url": "/burger/static/js/2.b4e27559.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/burger/static/js/2.b4e27559.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7af92d8e0ff22f7915a8",
+    "url": "/burger/static/js/main.da1573e4.chunk.js"
+  },
+  {
+    "revision": "81bebead0622ae598a73",
+    "url": "/burger/static/js/runtime-main.555648b6.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
